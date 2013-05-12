5d68854b11325a7b6a8d35347980495dec28756a
Geoffrey Bugaisky <gbugaisk@ucsd.edu>
2013-05-12 14:40:54 -0700

    Finish error check (P)

--- perceptron.py
+++ perceptron.py
@@ -21,10 +21,23 @@ def perceptron(trainfile, testfile):
 	print w
 
 	#get the error for the algorithm (Open testfile, and run)
+	error = 0
+	linecount = 0
 	with open(testfile, 'r') as test:
 		for line in test:
+			linecount += 1
 			templine = map(int, line.split())
 			label = templine[len(templine) - 1]
+			featurevec = templine[:-1]
+			if label == 0:
+				label = -1
+			else:
+				label = 1
+			if (label * np.dot(w, featurevec)) <= 0:
+				error += 1
+
+	#print out the error
+	print "Error on perceptron:", error, "/", linecount
 
 if __name__ == "__main__":
 	perceptron(".\\hw4train.txt", ".\\hw4train.txt")
