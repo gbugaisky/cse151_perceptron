e44f8b1f3fe119767bc192ebe2c7690bea1246ab
Geoffrey Bugaisky <gbugaisk@ucsd.edu>
2013-05-12 15:48:36 -0700

    Problem with Average.  Basic and Voted working fine.

--- perceptron.py
+++ perceptron.py
@@ -78,15 +78,57 @@ def votedperceptron(trainfile, testfile):
 			else:
 				label = 1
 			summation = 0
+
+			#NOTE: copysign gives us the sign of the equation (we use 1 to fit our label)
 			for element in vote:
 				summation += (element[1] * math.copysign(1, np.dot(element[0], featurevec)))
-			if copysign(1, summation) != label:
+			if math.copysign(1, summation) != label:
 				error += 1
 	print "Error on voted perceptron:", error, "/", linecount
 
-
 def averageperceptron(trainfile, testfile):
-	pass
+	#create the average perceptron classifier (var aver)
+	w = [0] * 784
+	c = 1
+	aver = [0] * 784#Blank list for holding later results
+	with open(trainfile, 'r') as file:
+		for line in file:
+			templine = map(int, line.split())
+			label = templine[len(templine) - 1]
+			featurevec = templine[:-1]
+			if label == 0:
+				label = -1
+			else:
+				label = 1
+
+			if (label * np.dot(w, featurevec)) <= 0:
+				temphold = [c * x for x in w]
+				aver = map(sum, izip(aver, temphold))
+				temparray = [label * x for x in featurevec]
+				w = map(sum, izip(w, temparray))
+				c = 1
+			else:
+				c += 1
+
+	print "Starting Error Check (This takes a while)..."
+	#get test error for average perceptron
+	error = 0
+	linecount = 0
+	with open(testfile, 'r') as test:
+		for line in test:
+			linecount += 1
+			templine = map(int, line.split())
+			label = templine[len(templine) - 1]
+			featurevec = templine[:-1]
+			if label == 0:
+				label = -1
+			else:
+				label = 1
+			
+			#do dot product and see if it equals label
+			if math.copysign(1, np.dot(aver, featurevec)) != label:
+				error += 1
+	print "Error on average perceptron:", error, "/", linecount
 
 if __name__ == "__main__":
-	votedperceptron(".\\hw4train.txt", ".\\hw4train.txt")
+	averageperceptron(".\\hw4train.txt", ".\\hw4train.txt")
